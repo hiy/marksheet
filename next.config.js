28efe4cf773fccefbae649aca15a4193a768897f
Hiroshi Yamasaki <ymskhrs@gmail.com>
2021-11-07 11:01:09 +0900

    Fix production base path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
+  env: {
+    basePath: isProduction ? '/timer-todo-front' : ''
+  },
   reactStrictMode: true,
+  basePath: isProduction ? '/timer-todo-front' : '',
 }
